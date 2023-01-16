ed63d982b9f93c42d5abfe468167d6ccd62b3b3c
CarlosDelgadoG <carlos.delgado@ug.uchile.cl>
2023-01-16 17:05:24 -0300

    flujo-pesos

--- master.R
+++ master.R
@@ -1,9 +1,24 @@
-source("C:/RProjects/ELSOC 2022/ponderadores-long/datos/codigo/0-crear bases long_wide.R")
+source("datos/codigo/0-crear bases long_wide.R")
 
-source("C:/RProjects/ELSOC 2022/ponderadores-long/datos/codigo/I-A-crear base imputada a partir de base long wide.R")
-source("C:/RProjects/ELSOC 2022/ponderadores-long/datos/codigo/I-B-Crear base proceso recoleccion datos.R")
-source("C:/RProjects/ELSOC 2022/ponderadores-long/datos/codigo/I-C-crear bases CIT.R")
+source("datos/codigo/I-A-crear base imputada a partir de base long wide.R")
+source("datos/codigo/I-B-Crear base proceso recoleccion datos.R")
+source("datos/codigo/I-C-crear bases CIT.R")
 
-source("C:/RProjects/ELSOC 2022/ponderadores-long/datos/codigo/II-crear base modelo.R")
+
+# MODELAMIENTO NR ---------------------------------------------------------
+
+
+source("datos/codigo/II-crear base modelo.R")
+
+
+# IMPUTAR VALORES EN BASE MODELO
+source("modelamiento NR/predicciones/III-A-crear base modelo con valores imputados.R")
+#HACER PREDICCION
+source("modelamiento NR/predicciones/III-B-generar predicciones nr base imputada.R")
+
+
+# GENERAR PESOS ----------------------------------------------------------------
+
+source("generar ponderadores/IV-generar master pesos.R")
 
 
