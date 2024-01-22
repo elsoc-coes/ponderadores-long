0f1113d1bd3348e1472820595b609eeaaffaa34e
Carlos Delgado González <carlosdelgadog93@gmail.com>
2024-01-22 15:05:44 -0300

    Update crear modelos gee con algoritmo backward.R

--- modelamiento NR/crear modelos gee con algoritmo backward.R
+++ modelamiento NR/crear modelos gee con algoritmo backward.R
@@ -138,13 +138,16 @@ back_gee <-function(base,vars,metrica,fijas=NULL){
 
 bestGEE_QICU<- back_gee(m1,covariables,"QICu")
 
-#saveRDS(bestGEE_QICU,file = "modelamiento NR/modelos/bestGEE_QICU.RDS",
-#     compress = "bzip2")
+saveRDS(bestGEE_QICU,file = "modelamiento NR/modelos/bestGEE_QICU.RDS",
+     compress = "bzip2")
 
 
 bestGEE_QICU_m2 <- back_gee(m2,covariables,"QICu")
-#saveRDS(bestGEE_QICU_m2,
- #       file = "modelamiento NR/modelos/bestGEE_QICU_m2.RDS",
-#        compress = "bzip2")
+saveRDS(bestGEE_QICU_m2,
+       file = "modelamiento NR/modelos/bestGEE_QICU_m2.RDS",
+        compress = "bzip2")
+
+
+
 
 
